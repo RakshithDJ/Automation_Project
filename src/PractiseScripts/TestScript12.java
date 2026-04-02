e0fdd8cb972c4f85c77fbb535ed4dd9f412652e9
learnings <rakshithdj13@gmail.com>
2026-04-02 17:03:40 +0530

    review TS12

--- src/PractiseScripts/TestScript12.java
+++ src/PractiseScripts/TestScript12.java
@@ -16,7 +16,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
  * Fluent wait
  ***/
 
-
+//Failing need to be reviewed
 class TestScript12 {
 
 	public static void main(String[] args) {
@@ -25,8 +25,8 @@ class TestScript12 {
 	driver.manage().window().maximize();
 	WebDriverWait W = new WebDriverWait(driver,Duration.ofSeconds(5));
 	driver.get("https://demoqa.com/");
-	W.until(ExpectedConditions.presenceOfElementLocated(By.linkText("Book Store Application"))).click(); 
-	W.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//*[text()='Book Store Application']/..//*[@fill='currentColor'])[2]"))).click();
+	W.until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//a[@href])[8]"))).click();
+	W.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("((//*[@stroke=\"currentColor\"])[41]"))).click();
 	W.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[text()='Login']"))).click();
 	System.out.println("Waits Executed");
 	driver.quit();

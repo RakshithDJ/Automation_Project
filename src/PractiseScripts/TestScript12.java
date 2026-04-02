820d0f63fe8ef75afbb7ee034a33a6a4e15a67a1
learnings <rakshithdj13@gmail.com>
2026-04-02 16:07:53 +0530

    Edited Script12

--- src/PractiseScripts/TestScript12.java
+++ src/PractiseScripts/TestScript12.java
@@ -1,8 +1,11 @@
 package PractiseScripts;
 
 import java.time.Duration;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 /***
@@ -22,6 +25,12 @@ class TestScript12 {
 	driver.manage().window().maximize();
 	WebDriverWait W = new WebDriverWait(driver,Duration.ofSeconds(5));
 	driver.get("https://demoqa.com/");
+	W.until(ExpectedConditions.presenceOfElementLocated(By.linkText("Book Store Application"))).click(); 
+	W.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//*[text()='Book Store Application']/..//*[@fill='currentColor'])[2]"))).click();
+	W.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[text()='Login']"))).click();
+	System.out.println("Waits Executed");
+	driver.quit();
+	
 
 		
 	}

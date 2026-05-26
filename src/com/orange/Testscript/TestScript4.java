e520749695fa9c8740dd084221c38caa666eba6d
learnings <rakshithdj13@gmail.com>
2026-05-26 15:47:35 +0530

    Updated creatAdminData() with generic excel methods

--- src/com/orange/Testscript/TestScript4.java
+++ src/com/orange/Testscript/TestScript4.java
@@ -1,9 +1,13 @@
 package com.orange.Testscript;
 
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
 import org.openqa.selenium.Keys;
 import org.testng.Reporter;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
+
 import com.orange.generic.BaseClass;
 import com.orange.generic.IRetryAnalyzerImplementation;
 import com.orange.pom.AdminPage;
@@ -14,27 +18,28 @@ public class TestScript4 extends BaseClass {
 	public AdminPage admin;
 
 	@Test(groups = "RegressionTest", retryAnalyzer = IRetryAnalyzerImplementation.class)
-	public void creatAdminData() {
+	public void creatAdminData() throws EncryptedDocumentException, IOException {
 		admin = new AdminPage(driver);
-		// admin.getAdminMenu().click();
-		admin.getUserNameTextbox().sendKeys("Ravi M B");
+		admin.getAdminMenu().click();
+		String un = lib.readExcelData("EMPDATA", 1, 0);
+		admin.getUserNameTextbox().sendKeys(un,Keys.ENTER);
 		admin.getUserRoleDropDown().click();
 		admin.getESSDropDownOption().click();
-		admin.getEmplyoeeNameTextbox().sendKeys("Ravi", Keys.ENTER);
+		String db = lib.readExcelData("EMPDATA", 1, 0);
+		admin.getEmplyoeeNameTextbox().sendKeys(db, Keys.ENTER);
 		admin.getRestButton().click();
 		Reporter.log("Clicked on Admin TestCase Pass " + ":" + "Regression TestMethod ", true);
 	}
 
-	@Test(groups = "SmokeTest", retryAnalyzer = IRetryAnalyzerImplementation.class)
-	public void creatAdminDatas() {
-		admin = new AdminPage(driver); //
-		admin.getAdminMenu().click();
-		admin.getUserNameTextbox().sendKeys("Ravi M B");
-		admin.getUserRoleDropDown().click();
-		admin.getESSDropDownOption().click();
-		admin.getEmplyoeeNameTextbox().sendKeys("Ravi", Keys.ENTER);
-		admin.getRestButton().click();
-		Reporter.log(" Unit Regression :Clicked on Admin TestCase Pass ", true);
-	}
+	/*
+	 * @Test(groups = "SmokeTest", retryAnalyzer =
+	 * IRetryAnalyzerImplementation.class) public void creatAdminDatas() { admin =
+	 * new AdminPage(driver); // admin.getAdminMenu().click();
+	 * admin.getUserNameTextbox().sendKeys("Ravi M B");
+	 * admin.getUserRoleDropDown().click(); admin.getESSDropDownOption().click();
+	 * admin.getEmplyoeeNameTextbox().sendKeys("Ravi", Keys.ENTER);
+	 * admin.getRestButton().click();
+	 * Reporter.log(" Unit Regression :Clicked on Admin TestCase Pass ", true); }
+	 */
 
 }
